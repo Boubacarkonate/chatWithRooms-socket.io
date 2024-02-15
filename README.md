729a96867a0167d70149da110bf6df8f8da6686c
Bouba <boubacar.konate@outlook.fr>
2024-02-15 11:24:24 +0100

    css ok + readme ajouté

---
+++ README.md
@@ -0,0 +1,22 @@
+# Chat-Room
+Création d'un chat avec Socket.io avec des rooms et une base de données
+## technologies 
+    - javascripts
+    - nodejs
+    - express
+    - MongoDB
+    - ejs
+    - html
+    - css
+
+## Mis en place
+### créer un fichier qui représentera le server du projet 
+    server.js 
+
+### initialisation de npm
+    npm init
+
+### installation des dépendances : express, mongoose, nodemon, ejs et socket.io
+    npm i express mongoose nodemon ejs socket.io
+   
+    
--- server.js
+++ server.js
@@ -14,5 +14,21 @@ let mongoose = require('mongoose');
 // Cela signifie que les fichiers HTML, CSS, JavaScript, etc., contenus dans ce répertoire seront accessibles à partir de l'URL racine de votre serveur (par exemple, http://localhost:8080/)
 app.use(express.static(__dirname + '/public'));
 
+// ROUTER
+// Définition d'une route pour l'URL racine '/' qui rend le fichier 'index.ejs'
+app.get('/', function (req, res) {
+    res.render('index.ejs');
+})
+
+// Middleware pour gérer les requêtes non trouvées (404)
+app.use(function (req, res, next) {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(404).send('ERREUR 404: Page introuvable !');
+})
+
+// IO
+// Importation du module Socket.IO et initialisation de l'écouteur de sockets sur le serveur HTTP
+let io = require('socket.io')(server);
+
 // Démarre le serveur HTTP sur le port 8080
 server.listen(8080, () => console.log('!!!! server started port: 8080 !!!!'));
